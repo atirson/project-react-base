9916a14839ee4bd971f9cd771f11c3983b0fd3d9
atirson <fabiano.oliveira2213@gmail.com>
2022-04-05 15:12:13 -0300

    config: Added package swc to config jest to run tests into react components

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,39 @@
 module.exports = {
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.dist/'],
   setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
-  },
-  moduleNameMapper: {
-    '\\.(scss|css|sass)$': 'identity-obj-proxy',
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: true,
+            decorators: true,
+          },
+          keepClassNames: true,
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+        module: {
+          type: 'es6',
+          noInterop: false,
+        },
+      },
+    ],
   },
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@app(.*)$': '<rootDir>/src$1',
+    '^@components(.*)$': '<rootDir>/src/components$1',
+    '^@core(.*)$': '<rootDir>/src/core$1',
+    '^@pages(.*)$': '<rootDir>/src/pages$1',
+    '^@assets(.*)$': '<rootDir>/src/assets$1',
+    '\\.(scss|css|sass)$': 'identity-obj-proxy',
+  },
 };
